f891d13a8fd29e0db2f045d202bd4a43440e1fd1
LuizAmancioCeub <luiz.amancio@sempreceub.com>
2025-03-19 16:14:44 -0300

    Simulando Conta bancária através do terminal

---
+++ src/contaBanco/ContaTerminal.java
@@ -0,0 +1,32 @@
+package contaBanco;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		
+		double saldo = 234.67;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Insira o nome do Cliente: ");
+		String nome= scanner.nextLine();
+		
+		System.out.println("Insira o número da Agência: ");
+		Integer agencia = scanner.nextInt();
+		
+		System.out.println("Insira o número da conta: ");
+		Integer numero = scanner.nextInt();
+		
+		String msg = "Olá "+nome+", obrigado por criar uma conta em nosso banco!"
+				+ "\r\nAgência: "+agencia
+				+ "\r\nConta: "+numero
+				+ "\r\nSaldo disponível: "+saldo;
+		
+		System.out.println(msg);
+
+	}
+
+}
